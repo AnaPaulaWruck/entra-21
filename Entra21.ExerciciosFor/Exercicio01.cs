a4754a021244a5ae81224b799b4011ec504b64ad
Ana Paula Wruck <wruck.ap@gmail.com>
2022-05-13 21:52:38 -0300

    Criado novo Projeto para Exercícios usando 'for' Criado novo project e iniciado Exercício01

---
+++ Entra21.ExerciciosFor/Exercicio01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosFor
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            // Solicite o preço e nome da peça de 13 peças.
+            Console.Clear();
+            var nome = "";
+            var preco = 0.0;
+            for (var i = 0; i < 3; i++)
+            {
+                while (nome.Length < 5)
+                {
+                    Console.Write("Digite o nome da peça: ");
+                nome = Console.ReadLine().Trim();
+                    Console.Write()
+                }
+                Console.Write("Digite o valor da peça: R$ ");
+                preco = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ Entra21.ExerciciosFor/Program.cs
@@ -0,0 +1,14 @@
+﻿using Entra21.ExerciciosFor;
+
+Console.WriteLine(@"
+-------MENU--------
+1 - Exercício 01");
+
+Console.Write("\nDigite a opção desejada: ");
+var opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+
+if (opcaoDesejada==1)
+{
+    Exercicio01 exercicio01 = new Exercicio01();
+    exercicio01.Executar();
+}
