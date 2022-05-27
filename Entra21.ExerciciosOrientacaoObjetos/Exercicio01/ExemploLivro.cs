38373f9674fecde8287af271725d3bbdc096ea7b
Ana Paula Wruck <wruck.ap@gmail.com>
2022-05-26 22:11:22 -0300

    Criado Exercicio 1 da lista de Exercícios Orientação a Objetos Criado Livro e ExemploLivro

---
+++ Entra21.ExerciciosOrientacaoObjetos/Exercicio01/ExemploLivro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjetos.Exercicio01
+{
+    public class ExemploLivro
+    {
+        public void Executar()
+        {
+            var livro = new Livro();
+            livro.Titulo = "Nunca coloque a mão de um cadáver na boca";
+            livro.Autor = "Dana Kollmann";
+            livro.CodigoIsbn = "978-65-89999-01-3";
+            livro.IdiomaOriginal = "Inglês";
+            livro.DataLancamento = new DateTime(2010, 06, 01);
+            livro.QuantidadePaginas = 265;
+            livro.QuantidadePaginasLidas = 52;
+            livro.QuantidadeReleituras = 2;
+
+            livro.ApresentarTituloAutor();
+            livro.ApresentarQuantidadePaginasParaLer();
+            livro.ApresentarQuantidadePaginasLidasTotal();
+        }
+    }
+}
---
+++ Entra21.ExerciciosOrientacaoObjetos/Exercicio01/Livro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjetos.Exercicio01
+{
+    public class Livro
+    {
+        public string Titulo;
+        public string Autor;
+        public string CodigoIsbn;
+        public string IdiomaOriginal;
+        public DateTime DataLancamento;
+        public int QuantidadePaginas;
+        public int QuantidadePaginasLidas;
+        public int QuantidadeReleituras;
+
+        public void ApresentarTituloAutor()
+        {
+            Console.WriteLine($"Título do livro: {Titulo}\nNome do autor: {Autor}");
+        }
+
+        public int CalcularQuantidadePaginasParaLer()
+        {
+            var quantidadePaginasParaLer = QuantidadePaginas - QuantidadePaginasLidas;
+
+            return quantidadePaginasParaLer;
+        }
+
+        public void ApresentarQuantidadePaginasParaLer()
+        {
+            Console.WriteLine($"Páginas pendentes para leitura: {CalcularQuantidadePaginasParaLer()}");
+        }
+
+        public void ApresentarQuantidadePaginasLidasTotal()
+        {
+            Console.WriteLine($"Quantidade de páginas lidas: {QuantidadePaginasLidas}");
+        }
+    }
+}
