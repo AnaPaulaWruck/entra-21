c7ea15120005255a6dc10d951298213565d979ac
Ana Paula Wruck <ana.wruck@gmail.com>
2022-06-06 20:23:14 -0300

    Criado projeto para Exercícios com Listas Adicionada classe com o Exercício 1

---
+++ Entra21.ExerciciosLists/Exercicio01.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosLists
+{
+    public class Exercicio01
+    {
+        public void Executar()
+        {
+            /*
+    Criar uma classe que irá armazenar os melhores filmes de todos os tempos.
+    a. Adicionar o filme "Dragon Ball Evollution";
+    b. Adicionar o filme "Titanic";
+    c. Adicionar o filme "Lanterna verde";
+    d. Listar os filmes armazenados na lista;
+    e. Adicionar o filme "";
+    f. Alterar o filme "Dragon Ball Evollution" para "Dragon Ball Super: Broly”;
+    g. Remover filme "Lanterna verde";
+    h. Verificar se a lista contém o filme "Lanterna verde" apresentando para o usuário se está ou não cadastrado;
+    i. Adicionar o filme "Quarteto fantástico";
+    j. Listar os filmes armazenados na lista;
+    k. Alterar o filme "Titanic" para “Titanic 2”;
+    l. Listar os filmes armazenados na lista;
+    m. Verificar se filme "A Lagoa Azul” existe na lista, informando para o usuário 
+            */
+
+            List<string> melhoresFilmes = new List<string>();
+            // a, b, c
+            melhoresFilmes.Add("Dragon Ball Evollution");
+            melhoresFilmes.Add("Titanic");
+            melhoresFilmes.Add("Lanterna Verde");
+            // d
+            Console.WriteLine("Filme 1: " + melhoresFilmes[0] + "\nFilme2: " + melhoresFilmes[1] + "\nFilme3: " + melhoresFilmes[2]);
+            // e
+            melhoresFilmes.Add("");
+            // f
+            melhoresFilmes[0] = "Dragon Ball Super: Broly";
+            // g
+            melhoresFilmes.Remove("Lanterna Verde");
+            // h
+            string filme = "Lanterna Verde";
+            bool contemFilme = melhoresFilmes.Contains(filme);
+            if (contemFilme == true)
+            {
+                Console.WriteLine("\nA lista contém o filme " + filme);
+            }
+            else
+            {
+                Console.WriteLine("\nA lista não contém o filme " + filme);
+            }
+            // i
+            melhoresFilmes.Add("Quarteto Fantástico");
+            // j
+            Console.WriteLine("\nFilme 1: " + melhoresFilmes[0] + "\nFilme 2: " + melhoresFilmes[1] + "\nFilme 3: " + 
+                melhoresFilmes[2] + "\nFilme 4: " + melhoresFilmes[3]);
+            // k
+            melhoresFilmes[1] = "Titanic 2";
+            // l
+            Console.WriteLine("\nFilme 1: " + melhoresFilmes[0] + "\nFilme 2: " + melhoresFilmes[1] + "\nFilme 3: " +
+                melhoresFilmes[2] + "\nFilme 4: " + melhoresFilmes[3]);
+            // m
+            filme = "A Lagoa Azul";
+            contemFilme = melhoresFilmes.Contains(filme);
+            if (contemFilme == true)
+            {
+                Console.WriteLine("\nA lista contém o filme " + filme);
+            }
+            else
+            {
+                Console.WriteLine("\nA lista não contém o filme " + filme);
+            }
+        }
+    }
+}
---
+++ Entra21.ExerciciosLists/Program.cs
@@ -0,0 +1,29 @@
+﻿using Entra21.ExerciciosLists;
+
+//Console.WriteLine(@"
+//-------MENU--------
+//1 - Exercício 01
+//2 - Exercício 02
+//3 - Exercício 03");
+
+//Console.Write("\nDigite a opção desejada: ");
+//var opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+
+//if (opcaoDesejada == 1)
+//{
+//    Exercicio01 melhoresFilmes = new Exercicio01();
+//    melhoresFilmes.Executar();
+//}
+//else if (opcaoDesejada == 2)
+//{
+//    Exercicio02 exercicio02 = new Exercicio02();
+//    exercicio02.Executar();
+//}
+//else if (opcaoDesejada == 3)
+//{
+//    Exercicio03 exercicio03 = new Exercicio03();
+//    exercicio03.Executar();
+//}
+
+Exercicio01 melhoresFilmes = new Exercicio01();
+melhoresFilmes.Executar();
